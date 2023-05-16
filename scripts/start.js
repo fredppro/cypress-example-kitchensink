bf47aee8e67469c704a3f6ee3f798afa3e69a58b
Frederico <fredp.profissional@gmail.com>
2023-05-16 20:06:06 +0100

    aaa

--- scripts/start.js
+++ scripts/start.js
@@ -13,4 +13,6 @@ console.log(`Running "${cmd}"...`)
 
 console.log("Quantas vezes preciso de fazer isto x4")
 
+console.log("Quantas vezes preciso de fazer isto x4")
+
 execSync(cmd, { stdio: 'inherit' })
